f55f010af5a25d1e61a1034bd5de258ebaa4a49f
Pavan-s-ux <pavans232004@gmail.com>
2025-04-11 16:45:50 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const locations = [
+    "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh",
+    "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jharkhand",
+    "Karnataka", "Kerala", "Madhya Pradesh", "Maharashtra", "Manipur",
+    "Meghalaya", "Mizoram", "Nagaland", "Odisha", "Punjab",
+    "Rajasthan", "Sikkim", "Tamil Nadu", "Telangana", "Tripura",
+    "Uttar Pradesh", "Uttarakhand", "West Bengal"
+  ];
+
+  const datalist = document.getElementById("location-suggestions");
+
+  locations.forEach(location => {
+    const option = document.createElement("option");
+    option.value = location;
+    datalist.appendChild(option);
+  });
